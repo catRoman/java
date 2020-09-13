0535a3ef32544d74f6c1868a55e858ee52e2e8d4
catRoman <cjroman@lakeheadu.ca>
2020-09-12 22:36:42 -0700

    added full directory and exercises

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is a Test");
-    }
+        System.out.println("Just testing within my eniviroment");
+  }
 }
