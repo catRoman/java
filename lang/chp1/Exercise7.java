0d15da3ad31d1c8f6b7979530d1b205f8c0648d1
catRoman <cjroman@lakeheadu.ca>
2020-09-13 19:30:33 -0700

    pi function created, program working, not comlete

--- lang/chp1/Exercise7.java
+++ lang/chp1/Exercise7.java
@@ -1,34 +1,41 @@
 import java.lang.Math.*;
  class Exercise7 {
-    public static void main(String[] args){
-        double pi1 =4 * 1.0 -(1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11);
-        double piExtended = 4 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11) + (1.0/13));
+
+     static void piFunction(int total){
+     double x = 1.0;
         double num = 1.0;
-        int total = 5;
-        double x = 1;
-        double xDen = 1;
+        double xDen = 1.0;
         double xNum = 1.0;
-     for(double i = 1.0; i <= total; i++) {
+     for(int i = 1; i <= total; i++) {
          // count number up to total by two
         
          if(i == 1.0){
-             xDen = 1.0;
+             x = 1.0;
             
          } else {
          xDen = (xDen + 2.0);
          }
-         if(i % 2 ==0){
-             xNum = xNum * (-1.0);
+         x = num * (xNum/xDen);
+         if(i % 2 == 0){
+             x = x * (-1);
          }
-         
-         Double pi = num * (xNum/xDen);
-         System.out.println("This is a test pi algorithm: " + pi);
+        
+         System.out.println("This is a test pi algorithm: " + x);
      }
-     
+       }
+
+    public static void main(String[] args){
+        double pi1 =4 * 1.0 -(1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11);
+        double piExtended = 4 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11) + (1.0/13));
+        int total = 5;
+       
+
+       
 
         
         System.out.println("This is pi: " + pi1);
         System.out.println("This is also pie: " + piExtended);
         System.out.println("This is the real Pi: " + Math.PI);
+        piFunction(total);
     }
 }
