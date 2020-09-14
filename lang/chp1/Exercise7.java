ddbbf34bd0a4c3a4a7676c364a3d68d9e381a7be
catRoman <cjroman@lakeheadu.ca>
2020-09-13 19:57:24 -0700

    Exercise 7(pi program) completed

--- lang/chp1/Exercise7.java
+++ lang/chp1/Exercise7.java
@@ -1,15 +1,18 @@
 import java.lang.Math.*;
  class Exercise7 {
 
-     static void piFunction(int total){
-     double x = 1.0;
-        double num = 1.0;
+     static double piFunction(int total){
+    
+        double x = 1.0;
+        double num = 4.0;
         double xDen = 1.0;
         double xNum = 1.0;
-     for(int i = 1; i <= total; i++) {
-         // count number up to total by two
+        double pi = 0;
+    
+         for(int i = 1; i <= total; i++) {
+             // count number up to total by two
         
-         if(i == 1.0){
+             if(i == 1.0){
              x = 1.0;
             
          } else {
@@ -19,23 +22,34 @@ import java.lang.Math.*;
          if(i % 2 == 0){
              x = x * (-1);
          }
-        
-         System.out.println("This is a test pi algorithm: " + x);
-     }
-       }
+
+        pi = pi + x;
+    }
+    return pi;
+    }
 
     public static void main(String[] args){
-        double pi1 =4 * 1.0 -(1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11);
+       
+        double pi1 =4 * (1.0 -(1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
         double piExtended = 4 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11) + (1.0/13));
         int total = 5;
-       
 
-       
-
-        
+        System.out.println("Through simple addition of 6 iterations,");
         System.out.println("This is pi: " + pi1);
-        System.out.println("This is also pie: " + piExtended);
+        System.out.println("This is with 7 iterations: " + piExtended);
+        System.out.println();
+        System.out.println("This is using the Math method provided by JVM,");
         System.out.println("This is the real Pi: " + Math.PI);
-        piFunction(total);
+        System.out.println();
+        System.out.println("This is through use of an Algorithm in a method I created");
+        System.out.println("This is pi to six iterations: " + piFunction(6));
+        System.out.println("This is pi to 7 iterations: " + piFunction(7));
+        System.out.println("This is pi to 8 iterations: " + piFunction(8));
+        System.out.println("This is pi to 9 iterations: " + piFunction(9));
+        System.out.println("This is pi to 10 iterations: " + piFunction(10));
+        System.out.println("This is pi to 100 iterations: " + piFunction(100));
+        System.out.println("This is pi to 250 iterations: " + piFunction(250));
+        System.out.println("This is pi to 1000 iterations: " + piFunction(1000));
+        System.out.println("This is pi to 10000 iterations: " + piFunction(10000));
     }
 }
