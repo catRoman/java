243ca815afacebf1f49f029c7094bb7e85b3e650
catRoman <cjroman@lakeheadu.ca>
2020-09-19 23:15:42 -0700

    exercise 1 complete

--- lang/chp2/Exercise2_1.java
+++ lang/chp2/Exercise2_1.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
 class Exercise2_1{
+static public void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
+System.out.print("Enter a degree in Celsius ");
+    double celsius = input.nextDouble();
+    double fahrenheit = (celsius * (9.0/5)) + 32;
     
+    System.out.println((int)celsius + " Celsius is " + fahrenheit + " Fahrenheit");
+}
 }
