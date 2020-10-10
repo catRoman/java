5660c4b342385c3ecc794958695367bd349eecfa
catRoman <cjroman@lakeheadu.ca>
2020-10-10 10:52:40 -0700

    yesterdays commits

---
+++ exercism/java/hello-world/vimfuckabout/Fuckabout.java
@@ -0,0 +1,11 @@
+public class Fuckabout
+{
+	public static void main(String[] args){
+		System.out.println("Hey what the fuck is all this then eh...");
+		System.out.println("testing out vim...");
+		System.out.println("no mouse all command line mother fucker...");
+				
+	
+	}
+
+}
