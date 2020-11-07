b933007e1f998db573c1548e579e41dace29e2d5
catRoman <cjroman@lakeheadu.ca>
2020-11-07 00:43:00 -0800

    desktop

--- Exercise7_20.java
+++ Exercise7_20.java
@@ -2,7 +2,7 @@
 The selection-sort method repeatedly finds the smallest number in the current
 array and swaps it with the first. Rewrite this program by finding the largest num-
 ber and swapping it with the last. Write a test program that reads in ten double
-numbers, invokes the method, and displays the sorted numbers. */
+numbers, invokes the method, and displays the sorted numbers. s*/
 
 import java.util.Scanner;
 
