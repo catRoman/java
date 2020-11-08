c18190b33441eb0d4932211993fc24ead46a1876
catRoman <cjroman@lakeheadu.ca>
2020-11-08 00:36:49 -0800

    nqueen firstdraft complete(backtracking)

--- lang/chp7/Exercise7_22.java
+++ lang/chp7/Exercise7_22.java
@@ -1,4 +1,4 @@
-
+import javax.lang.model.util.ElementScanner6;
 
 /**(Game: Eight Queens) The classic Eight Queens puzzle is to place eight queens
 on a chessboard such that no two queens can attack each other (i.e., no two queens
@@ -16,36 +16,58 @@ shown below:
 | | | |Q| | | | |
 
 */
-
+// first glimpse of backtracking and recursion, not used effiecently but its a start, once 
+// a solution was not possible the system restarts from the the beginning untill a solution is 
+// aquired rather than reversing one iteration at a time to check for solutions....
+// ill probly be back here to change it later down the roadS
 public class Exercise7_22{
     public static void main(String[] args){
 
-        int[] usedColumns = new int[8];
-        java.util.Arrays.fill(usedColumns, -1);
-
+        // declare boolean 2 x 2 matrix and fill with true
+        // the represent space available
         boolean[][] available = new boolean[8][8];
+        int queensPlaced = 0; //for backtracking
+        int[] queensPosition = new int[8];
+        
+        //master backtrack loop
+        do{
         
         for(int i =0; i < available.length; i++)
             java.util.Arrays.fill(available[i], true);
-        
+        queensPlaced = 0;
         
 
+        // main game iterations    
         for(int i = 0; i < 8; i++){
            // while(availableSpace(available[i])){
-                int position = availableRandomNumber(available, i);
-                System.out.print(position);
+
+                // place a queen randomly in an available space
+                int position = availableRandomSpace(available, i);
+                
+                //if you can no longer place a queen, restart
+                if(position < 0){
+                    break;
+                }else
+                    queensPlaced++;
+                    queensPosition[i] = position; // used for displaying Queens
+               
 
                 for(int j = 0; j < 8; j++){
-                   available[j][position]= false; // columns
-                    diagonal(available,position, i, j);
+                    // update non availibly spaces throughout
+                    // rest of board after position is selected
+                    available[j][position]= false; // columns
+                    diagonal(available,position, i, j); //diagonals
                 }
-                chessBoardRow(i,position, available);
+                //display chess board row
+               // displayChessBoardRow(i,position, available);
         
           
                 //System.out.println();
-            
-    }
+            }    
+    }while(queensPlaced < 8);
+    displayChessBoard(available, queensPosition);
 }
+//first attempt at backtrack loop
     public static boolean availableSpace(boolean[] availableSpace){
         boolean key = true;
         for(int i =0; i < availableSpace.length; i++){
@@ -55,6 +77,7 @@ public class Exercise7_22{
         return false;
     } 
 
+    // diagonals left and right down from current row
     public static void diagonal(boolean[][] available, int position, int startingRow, int currentRow){
     int rowChange = currentRow - startingRow;
 
@@ -67,39 +90,44 @@ public class Exercise7_22{
     }
     
 }
-    public static int availableRandomNumber(boolean[][] available, int row){
+//creates a random index number until the number lies on any assioceated
+// available space
+    public static int availableRandomSpace(boolean[][] available, int row){
        int number = -1;
-      
+      if(availableSpace(available[row])){
     do{
        number = (int)(Math.random() * 8);    
     }while(available[row][number] == false);
         return number;
+    }else 
+        return -1;
     }
 
     /** takes in a boolean  value if a particular space in a row is 
      * occupied and either displays a queen of a space, drawing
-     * a 8 x 8 board
+     * a 1 x 8 line, in ascoitiation with a noter loop, a full
+     * 8 x 8 chess board is produced
      * 
     */
-    public static void chessBoardRow(int row,int position, boolean[][] available){
-        System.out.print("|");
+    public static void displayChessBoard(boolean[][] board, int[] queensPosition){
+        
        
+
         for(int i = 0; i < 8; i++){
-                
-                if(i == position)
+            System.out.print("|");
+                for(int j = 0; j < 8; j++){
+                if(j == queensPosition[i])
                     System.out.print("Q");
-                else if(available[row][i] == false)
-                    System.out.print("*");
+                
                 else
                     System.out.print(" ");
-
-                System.out.print("|"); 
-               
-
+                    System.out.print("|");
+                }
+                System.out.println();
         }
 
 
-        System.out.println();
+       
 
         
     
