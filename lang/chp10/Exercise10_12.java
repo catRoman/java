8428972801c6f8aaa43e3193b993b6f94669b183
Cat Roman <cjroman@lakeheadu.ca>
2020-12-04 00:46:58 -0800

    laptop

--- lang/chp10/Exercise10_12.java
+++ lang/chp10/Exercise10_12.java
@@ -1,3 +1,18 @@
 public class Exercise10_12 {
-    
+    public static void main(String[] args){
+        Triangle2D t1 = new Triangle2D(new MyPoint(2.5,2),
+                        new MyPoint(4.2,3), new MyPoint(5,3.5));
+        
+        System.out.println("area: " + t1.getArea());
+        System.out.println("perimeter: " + t1.getPerimeter());
+        System.out.println();
+        System.out.println("contains: " + t1.contains(new MyPoint(3,3)));
+        System.out.println("contains: " + t1.contains(new Triangle2D(new MyPoint(2.9,2), 
+                                                                    new MyPoint(4,1),
+                                                                    new MyPoint(1,3.4))));
+        System.out.println();
+        System.out.println("overlap: " + t1.overlaps(new Triangle2D(new MyPoint(2,5.5), 
+                                                                    new MyPoint(4,-3),
+                                                                    new MyPoint(2,6.5))));
+    }    
 }
--- lang/chp10/Triangle2D.java
+++ lang/chp10/Triangle2D.java
@@ -64,15 +64,40 @@
 
         double s = (side1 + side2 + side3)/2;
  
-        double area = Math.sqrt((s*(s-side1)*(s-side2)*(s-side3)));
+        return Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
         
-        return (int)(area*10)/10.0;
+        
         
      }
      public double getPerimeter(){
          return p1.distance(p2) + p2.distance(p3) + p3.distance(p1);  
      }
      public boolean contains(MyPoint p){
+        Triangle2D t1 = new Triangle2D(p, p1, p2);
+        Triangle2D t2 = new Triangle2D(p,p1,p3);
+        Triangle2D t3 = new Triangle2D(p,p2,p3);
+
+        if(t1.getArea()+t2.getArea()+t3.getArea() == this.getArea()){
+            return true;
+        }else
+            return false;
+    
+    }
+    public boolean contains(Triangle2D t){
+        if(this.contains(t.getP1()) && this.contains(t.getP2()) && this.contains(t.getP3())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean overlaps(Triangle2D t){
+        if(this.contains(t.getP1()) || this.contains(t.getP2()) || this.contains(t.getP3())){
+            return true;
+        }else{
+            return false;
+        }
+    
+    }
         
 
  }
