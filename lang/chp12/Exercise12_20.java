3a57391fd4ba9e5d404978809ad4cc6b83b2d640
Cat Roman <cjroman@lakeheadu.ca>
2021-01-07 14:01:29 -0800

    laptop

--- lang/chp12/Exercise12_20.java
+++ lang/chp12/Exercise12_20.java
@@ -1,5 +1,12 @@
 public class Exercise12_10{
     public static void main(String[] args){
         
+        checkDirectory(srcRootDirectory);
+        removePackage(srcRootDirectory); 
     }
+    if(!agrs.length == 1){
+        System.out.println("Usage: java Exercise12_20 [srcRootFile]");
+        System.out.exit(1);
+    }
+   File srcRootDirectory = new File(args[0]);
 }
