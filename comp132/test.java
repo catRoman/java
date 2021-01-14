366a49d72b15188a77363c6528c4b13322fc5679
Cat Roman <cjroman@lakeheadu.ca>
2021-01-14 08:47:10 -0800

    laptop

--- comp132/test.java
+++ comp132/test.java
@@ -1,10 +1,35 @@
-public Test{
-    public static void main(String[] args){
+import java.util.Arrays;
+import java.util.Scanner;
 
-        int[][] test = new int[6][6];
+public class Test{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int[][] test = new int[12][4];
 
         Lab6.fillSeatsRandomly(test);
 
-        System.out.println(java.util.Arrays.toDeepString(test));
+        System.out.print("Check for availability: ");
+        int seatRow = input.nextInt();
+        char seatColumn = input.next().charAt(0);
+       
+        Lab6.isSeatAvailable(seatRow, seatColumn);
+
+        System.out.println();
+        System.out.println();
+        Lab6.displaySeats(test);
+
+        System.out.print("Which seat would you like to reserve?: ");
+        seatRow = input.nextInt();
+        seatColumn = input.next().charAt(0);
+       
+        Lab6.reserveSeat(seatRow, seatColumn);
+
+        System.out.println();
+        System.out.println();
+
+        Lab6.displaySeats(test);
+
+        
     }
 }
