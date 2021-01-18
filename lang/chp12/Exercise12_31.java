3f365c7f9fde4a303c6edc67dfd8da114a41b7af
catRoman <cjroman@lakeheadu.ca>
2021-01-17 16:48:18 -0800

    getting there

--- lang/chp12/Exercise12_31.java
+++ lang/chp12/Exercise12_31.java
@@ -2,6 +2,7 @@ import java.net.URL;
 import java.net.MalformedURLException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class Exercise12_31{
     public static void main(String[] args){
@@ -9,18 +10,18 @@ public class Exercise12_31{
         System.out.print("Enter the year: ");
         String year= input.nextLine();
         System.out.print("Enter the gender: ");
-        char gender = input.charAt(0);
-        System.out.println("Enter the name: ");
+        char gender = input.nextLine().charAt(0);
+        System.out.print("Enter the name: ");
         String name= input.nextLine();
 
         
-    String site = "http://liveexample.pearsoncmg.com/data/babynamesranking";
-    ArrayList<String> maleNames = new ArrayList<>();
-    ArrayList<Integer> maleNum = new ArrayList<>();  
-    ArrayList<String> femaleNames = new ArrayList<>();
-    ArrayList<Integer> femaleNum = new ArrayList<>();
+        String site = "http://liveexample.pearsoncmg.com/data/babynamesranking";
+        ArrayList<String> maleNames = new ArrayList<>();
+        ArrayList<Integer> maleNum = new ArrayList<>();  
+        ArrayList<String> femaleNames = new ArrayList<>();
+        ArrayList<Integer> femaleNum = new ArrayList<>();
 
-    try{
+        try{
             URL url = new URL(site + year + ".txt");
             Scanner siteInput = new Scanner(url.openStream());
 
@@ -31,5 +32,36 @@ public class Exercise12_31{
                 femaleNames.add(siteInput.next());
                 femaleNum.add(siteInput.nextInt());
             }
+        }catch(MalformedURLException ex){
+        System.out.println("Error: invaid URL");
+        }catch(IOException ex){
+        System.out.println("Error: invalid I/O");
+    }
+        boolean status = false;
+        if(gender == 'M' || gender == 'm'){
+            for(int i = 0; i < maleNames.size(); i++){
+                if(maleNames.get(i).toLowerCase().equals(name.toLowerCase())){
+                    System.out.println(name + " is ranked #" + (i +1)
+                    + " in the year " + year);
+                    System.out.println(maleNum.get(i) + " were named this year"); 
+                    status =true;
+                }
+            }
+            if(!status)
+                System.out.println(name + " is not ranked in " + year);
+        }else if(gender == 'F' || gender == 'f'){
+        for(int i = 0; i < femaleNames.size(); i++){
+            if(femaleNames.get(i).toLowerCase().equals(name.toLowerCase())){
+                System.out.println(name + " is ranked #" + (i +1)
+                + " in the year " + year);
+                System.out.println(femaleNum.get(i) + " were named this year");
+                status = true;
+            }
+        }
+        if(!status)
+            System.out.println(name + " is not ranked in " + year);
+        }else{
+    System.out.println("Gender not recognized");
+}
     }
 }
