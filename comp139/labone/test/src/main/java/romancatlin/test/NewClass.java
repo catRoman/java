c3b5f2e25b39d7787ec8ad8d825c07da7b495bf3
Catlin Roman <catRoman@localhost.localdomain>
2021-01-28 21:06:56 -0800

    staged all new modifications-Desktop-fast push -a

---
+++ comp139/labone/test/src/main/java/romancatlin/test/NewClass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package romancatlin.test;
+
+/**
+ *
+ * @author catRoman
+ */
+
+public class NewClass {
+  
+    public NewClass(){
+    }
+    public String test(){
+        
+        return "Hi\n" + 
+    
+        "\nljkdfhg "
+        + "\nThis is a testg";
+    }
+    @Override
+    public String toString(){
+        return "this is a another test";
+    }
+        
+}
