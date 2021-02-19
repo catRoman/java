b181ad9d6d9b42be758e7514b624b7a8b02c00fc
Catlin Roman <catRoman@localhost.localdomain>
2021-02-18 20:24:11 -0800

    staged all new modifications-Desktop-fast push -a

--- headfirst/DesignPatterns/src/chp1/designPuzzle/game.java
+++ headfirst/DesignPatterns/src/chp1/designPuzzle/game.java
@@ -1,5 +1,9 @@
 package chp1.designPuzzle;
 
+import chp1.designPuzzle.characters.King;
+
 public class game {
-    
+    public static void mian(String[] args){
+        Character[] roster = {new King(1000), }
+    }    
 }
